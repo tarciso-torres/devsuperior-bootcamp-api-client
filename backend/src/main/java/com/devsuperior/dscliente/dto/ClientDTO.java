b9f17aed4cc96e0182006edef75ffe7995a1f9a7
Tarciso Torres <tarcisoftorres@gmail.com>
2021-05-17 15:21:29 -0300

    Create Client DTO

---
+++ backend/src/main/java/com/devsuperior/dscliente/dto/ClientDTO.java
@@ -0,0 +1,76 @@
+package com.devsuperior.dscliente.dto;
+
+import java.time.Instant;
+
+import com.devsuperior.dscliente.entities.Client;
+
+public class ClientDTO {
+
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+
+	public ClientDTO(Client client) {
+		this.id = client.getId();
+		this.name = client.getName();
+		this.cpf = client.getCpf();
+		this.income = client.getIncome();
+		this.birthDate = client.getBirthDate();
+		this.children = client.getChildren();
+	}
+
+	public ClientDTO() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+}
